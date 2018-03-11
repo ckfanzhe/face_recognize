eb99b01b90afeba447ee2e02e61ad211e734fc10
ckfanzhe <kvy007@qq.com>
2018-03-11 17:18:12 +0800

    更新3.11
    对模型的运用代码进行了更新，使其能够对用于多个人脸进行分类

--- employ.py
+++ employ.py
@@ -18,11 +18,18 @@ def img_input(file_path):
     image = np.array(imag)
     return image
 
+
+def put_text(img, index):
+    text = target_name[index] + str(round(prediction[:, index][0] * 100, 2)) + '%'
+    print(text)
+    img = cv2.putText(img, text, (x2, x1), font, 0.7, (255, 255, 255), 2)
+    return img
+
 # 测试图片 #
 def evaluate_image(image_array, model_dir):
     with tf.Graph().as_default():
         batch_size = 1  # 一次传入一张图片
-        n_classes = 2  # 最后分类的结果有多少种 #
+        n_classes = 4  # 最后分类的结果有多少种 #
         image = tf.cast(image_array, tf.float32)
         image = tf.image.per_image_standardization(image)
         image = tf.reshape(image, [1, 64, 64, 3])
@@ -47,11 +54,13 @@ def evaluate_image(image_array, model_dir):
             prediction = sess.run(logit, feed_dict={x: image_array, dropout_half: 1.0, dropout_quart: 1.0})
             return prediction, global_step
 
+
 if __name__ == '__main__':
-    video_dir = 'E:/rootcoding/tensorflow/face_recognize/data/video_data/video2.mp4'  # 需要进行人脸识别的视频
-    model_dir = 'E:/models/model_2_11_2'  # 模型调用路径
-    target_name = ['hui', 'Unknown']  # 最后识别出来的标签1,2
-    out = cv2.VideoWriter('out.avi', -1, 10.0, (1080, 608))  # 注:在此修改视频保存尺寸
+    video_dir = 'E:/data/video_data/ssdgyfz.mp4'  # 需要进行人脸识别的视频
+    model_dir = 'E:/models/model_3_11'  # 模型调用路径
+    target_name =  ['shanxia', 'gonqi','zhushou','faces_other' ]  # 最后识别出来的标签1,2,3,4
+    out = cv2.VideoWriter('out1.avi', -1, 10.0, (1280, 720))  # 注:在此修改视频保存尺寸
+    font = cv2.FONT_HERSHEY_SIMPLEX  # 使用默认字体
     while True:
         print('--------------------------------------------------------')
         print('-                1 .开始摄像人脸识别                   -')
@@ -59,18 +68,17 @@ if __name__ == '__main__':
         print('-                3 .视频人脸识别并保存识别结果          -')
         print('-                4 .退出程序                           -')
         print('--------------------------------------------------------')
-        cmd = str(input("请输入一个选项："))
         detector = dlib.get_frontal_face_detector()
         size = 64
+        cmd = str(input("........>"))
         if cmd == '1':
             cam = cv2.VideoCapture(0)
         elif (cmd == '2')or(cmd == '3' ):
-            print('2')
             cam = cv2.VideoCapture(video_dir)
         elif cmd == '4':
             break
         else:
-            print("Error..")
+            print("请在上述的选项中进行输入！")
             continue
         ret = 0
         while (ret==0):
@@ -95,18 +103,8 @@ if __name__ == '__main__':
                 print(prediction)
                 max_index = np.argmax(prediction)  # 取结果中最大的
                 cv2.rectangle(img, (x2, x1), (y2, y1), (0, 255, 0), 3)  # opencv里的BGR
-                if max_index == 0:
-                    font = cv2.FONT_HERSHEY_SIMPLEX  # 使用默认字体
-                    text = target_name[0] + str(round(prediction[:, 0][0] * 100, 2)) + '%'
-                    print(text)
-                    img = cv2.putText(img, text, (x2, x1), font, 0.7, (255, 255, 255), 2)  # 图片/添加的文字/左上角坐标/字体/字体大小/颜色/字体粗细
-                elif max_index == 1:
-                    font = cv2.FONT_HERSHEY_SIMPLEX
-                    text = target_name[1] + str(round(prediction[:, 1][0] * 100, 2)) + '%'
-                    print(text)
-                    img = cv2.putText(img, text, (x2, x1), font, 0.7, (255, 255, 255), 2)
+                img = put_text(img, max_index)
                 if cmd == '3':
-                    # frame = cv2.flip(img,None)
                     out.write(img)
                     cv2.imshow('frame', img)
                     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -115,7 +113,7 @@ if __name__ == '__main__':
                 else:
                     cv2.imshow('There have some face', img)
                     if cv2.waitKey(1) & 0xFF == ord('q'):  # 按q键退出
-                        sys.exit(0)
+                        ret += 1 # 用于跳出本次循环
 
         out.release()
         cv2.destroyAllWindows()
